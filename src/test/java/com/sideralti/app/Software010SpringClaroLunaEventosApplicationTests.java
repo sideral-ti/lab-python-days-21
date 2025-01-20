1bd49a63f12aa9f5e812770922157def45cf2814
Sideral TI <sideralti.app@gmail.com>
2025-01-20 14:54:48 -0300

    initial project

---
+++ src/test/java/com/sideralti/app/Software010SpringClaroLunaEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Software010SpringClaroLunaEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
