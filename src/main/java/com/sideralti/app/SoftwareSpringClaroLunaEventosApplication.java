11a382fb15d5e57e3a168a3396897b01c005e124
Sideral TI <sideralti.app@gmail.com>
2025-01-23 13:46:02 -0300

    initial project

---
+++ src/main/java/com/sideralti/app/SoftwareSpringClaroLunaEventosApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSpringClaroLunaEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSpringClaroLunaEventosApplication.class, args);
+	}
+
+}
